1088ccf15d398f45cae167f83088fb04d758f198
Damián Clancig <damian@clancig.com.ar>
2022-09-30 16:48:31 -0300

    Se agrega funcionalidad de buscador de productos por titulo o descripción.

--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Product from '../components/Product'
+import SearchBar from '../components/SearchBar';
 
 import './productList.css'
 
-const ProductList = (props) => {
+const ProductList = ({products}) => {
+  const [ searchValue, setSearchValue ] = useState('');
+  
+  let searchedProducts = [];
+  
+  if (!searchValue.length >= 1) {
+    searchedProducts = products;
+  }
+  else {
+    const searchedProductsTitle = products.filter(product => {
+      const productTitle = product.title.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return productTitle.includes(searchText);
+    });
+    const searchedProductsDesc = products.filter(product => {
+      const productDescription = product.shortDescription.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return productDescription.includes(searchText);
+    })
+    searchedProducts = [...new Set([...searchedProductsTitle, ...searchedProductsDesc])];
+  }
+
+
   return (
-    <div className="productsList">
-      {
-        props.products.map((prod, index) => {
-          return <Product key={"product_" + index} prod={prod} />
-        })
-      }
-    </div>
+    <>
+      <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+      <div className="productsList">
+        {
+          searchedProducts.map((prod, index) => {
+            return <Product key={"product_" + index} prod={prod} />
+          })
+        }
+      </div>
+    </>
   );
 }
 
