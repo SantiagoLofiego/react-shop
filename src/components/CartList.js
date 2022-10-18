261b7e236513037bbb84754879bbb5af9c9a6b7a
Wilson Martinez <wm25289@gmail.com>
2022-10-18 15:11:54 -0300

    carrito-funcionalidad-estilos-carList

--- src/components/CartList.js
+++ src/components/CartList.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import CartItem from "./CartItem";
+import CartEmpty from "./CartEmpty";
+import CartTotal from './CartTotal';
+import Button from "react-bootstrap/Button";
+import "../containers/ShoppingCart.css";
 
-const CartList = () => {
+const CartList = (props) => {
   return (
-    <div>CartList</div>
+    <div className="containerCart">
+          <div className="cartHeader w-100 h-100">
+            <h3>Detalle Carrito</h3>
+            <h2
+              className="closetCart"
+              title="Cerrar"
+              onClick={() => props.setActiveCart(!props.isActiveCart)}
+            >
+              X
+            </h2>
+          </div>
+          {props.cart.length > 0 ? (
+            <div className="bodyCart">
+              {props.cart.map((prod, index) => {
+                return (
+                  <CartItem key={prod.id + index} prod={prod}  cartDispatcher={props.cartDispatcher}/>
+                );
+              })}
+              <CartTotal total={props.total} productLength={props.productLength} />
+              <Button
+                className="mb-3"
+                variant="primary"
+                title="Confirma tu compra"
+              >
+                Confirmar compra
+              </Button>
+            </div>
+
+          ) : (
+
+            <CartEmpty setActiveCart={props.setActiveCart}/>
+
+          )}
+        </div>
   )
 }
 
