c1ce38744ce9c5cc7e9e0ade832c59892134266a
Santiago Lofiego <santiagolofiego@gmail.com>
2022-11-03 10:59:37 -0300

    Se agrega paginación a la lista de productros, se limpia el index.html
    -Se agrega funcionalidad de paginacion a la lista de producto con un maximo de 12 productos
    -Se agrega efecto fadein a los cambios entre páginas en la lista de Ordenes y en la lista de productos
    -Se limpia el archivo index.html de los comentarios por defecto de react
    -Se modifican los min-width de los items de la lista de ordenes para un mejor comportamiento del responsive

--- src/components/OrdersList.js
+++ src/components/OrdersList.js
@@ -5,6 +5,7 @@ const OrdersList = ({ handleCheckOrder, error, orders, loading }) => {
   const [pagOrders, setPageOrders] = useState([]);
   const [activePage, setActivePage] = useState(1);
   const [pages, setPages] = useState([]);
+  const [fade, setFade] = useState('')
 
   useEffect(() => {
     const maxItems = 5;
@@ -19,25 +20,33 @@ const OrdersList = ({ handleCheckOrder, error, orders, loading }) => {
     setPageOrders(newOrders);
   }, [activePage, orders])
 
+  const handleChangePage = (number) => {
+    setFade('anim-pagination-start')
+    setActivePage(number);
+    setTimeout(() => setFade('anim-pagination-end'), 50)
+  }
+
   return (
     <div className='order-list mt-3'>
       <h2 className="h2">Pedidos Realizados</h2>
       {error ? <h4>{error}</h4> : ''}
-      {orders.length < 1 && !loading? <h3 className='h3'>No ha realizado ningun pedido por el momento</h3> : ''}
-      {loading? <Spinner animation="border" variant="primary" />:null}
-      <div className='p-2 mx-auto mw'>
-        {pagOrders.map(order => {
-          return (
-            <div key={order.fid} className='d-flex flex-wrap alert alert-success m-auto mt-2 order'>
-              <div className="p-2 text-start flex-fill">Order ID: {order.fid}</div>
-              <div className="p-2 text-start flex-fill">Fecha de compra: {new Date(order.date.seconds * 1000).toLocaleDateString()}</div>
-              <button onClick={() => handleCheckOrder(order.fid)} className="btn btn-primary">Ver Compra</button>
-            </div>
-          )
-        })}
+      {orders.length < 1 && !loading ? <h3 className='h3'>No ha realizado ningun pedido por el momento</h3> : ''}
+      {loading ? <Spinner animation="border" variant="primary" /> : null}
+      <div className={`p-2 mx-auto mw`}>
+        <div className={fade}>
+          {pagOrders.map(order => {
+            return (
+              <div key={order.fid} className='d-flex flex-wrap alert alert-success m-auto mt-2 order'>
+                <div className="p-2 text-start flex-fill order-id">Order ID: {order.fid}</div>
+                <div className="p-2 text-start flex-fill order-date">Fecha de compra: {new Date(order.date.seconds * 1000).toLocaleDateString()}</div>
+                <button onClick={() => handleCheckOrder(order.fid)} className="btn btn-primary">Ver Compra</button>
+              </div>
+            )
+          })}
+        </div>
         <Pagination className='mt-2'>
           {pages.map((number) => {
-            return (<Pagination.Item onClick={() => setActivePage(number)} key={number} active={number === activePage}>{number}</Pagination.Item>)
+            return (<Pagination.Item onClick={() => handleChangePage(number)} key={number} active={number === activePage}>{number}</Pagination.Item>)
           })}
         </Pagination>
       </div>
--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,38 +1,77 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from '../components/Product'
 import SearchBar from '../components/SearchBar';
 import { useProducts } from '../hooks/useProducts';
 import { BsDot } from 'react-icons/bs';
 import '../styles/productList.css'
 import { useMountAnimation } from '../hooks/useMountAnimation';
+import { Pagination } from 'react-bootstrap';
 
 const ProductList = () => {
   const { products } = useProducts();
   const [searchValue, setSearchValue] = useState('');
   const glassPic = 'https://firebasestorage.googleapis.com/v0/b/react-shop-90ad9.appspot.com/o/qwe%2Flupa.png?alt=media&token=46bf6032-f61b-4b0b-a601-c81cc8fdaa42';
   const animated = useMountAnimation('anim-fade-start', 'anim-fade-end');
+  const [searchedProducts, setSearchedProducts] = useState(products)
+  const [pagProducts, setPageProducts] = useState([]);
+  const [activePage, setActivePage] = useState(1);
+  const [pages, setPages] = useState([]);
+  const [fade, setFade] = useState('')
 
-  let searchedProducts = [];
-  if (!searchValue.length >= 1) {
-    searchedProducts = products;
-  }
-  else {
-    searchedProducts = products.filter(product => {
-      const productTitle = product.title.toLowerCase();
-      const productDescription = product.shortDescription.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-      return productTitle.includes(searchText) || productDescription.includes(searchText);
-    });
+  useEffect(() => {
+    if (!searchValue.length >= 1) {
+      setSearchedProducts(products);
+    }
+    else {
+      const newProductList = products.filter(product => {
+        const productTitle = product.title.toLowerCase();
+        const productDescription = product.shortDescription.toLowerCase();
+        const searchText = searchValue.toLowerCase();
+        return productTitle.includes(searchText) || productDescription.includes(searchText);
+      });
+      setSearchedProducts(newProductList);
+    }
+  }, [searchValue, products])
+
+  useEffect(() => {
+    let maxItems=12;
+    const pagesCount = Math.ceil((searchedProducts.length / maxItems));
+    const newPages = []
+    for (let i = 1; i <= pagesCount; i++) {
+      newPages.push(i);
+    }
+    const start = (activePage - 1) * maxItems;
+    const newProductList = searchedProducts.slice(start, start + maxItems)
+    setPages(newPages)
+    setPageProducts(newProductList);
+  }, [activePage, searchedProducts])
+
+  const handleChangePage = (number) => {
+    setFade('anim-pagination-start')
+    setActivePage(number);
+    setTimeout(()=>setFade('anim-pagination-end'),50)
   }
 
   return (
     <div className={animated}>
       <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
-      <div className="productsList">
+
+
+
+      <div className="container">
         {searchedProducts.length > 0 ?
-          searchedProducts.map((prod, index) => {
-            return <Product key={"product_" + index} prod={prod} />
-          })
+          <>
+            <div className={`productsList ${fade}`} >
+              {pagProducts.map((prod, index) => {
+                return <Product key={"product_" + index} prod={prod} />
+              })}
+            </div>
+            <Pagination>
+              {pages.map((number) => {
+                return (<Pagination.Item onClick={() => handleChangePage(number)} key={number} active={number === activePage}>{number}</Pagination.Item>)
+              })}
+            </Pagination>
+          </>
           :
           <div className='container m-2 shadow-sm p-3 mb-5 bg-body rounded d-flex'>
             <div className='p-2 flex-shrink-1'>
